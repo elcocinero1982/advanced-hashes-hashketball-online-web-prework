96a388ac770d3603c08e4330e24b1a456c5a7131
jose luis <cortezluisjose@gmail.com>
2019-07-06 03:45:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,8 @@
 # Write your code here!
  def game_hash
   game_hash[:home][:away]
-   
+   :colors => [{}]
+   :players => [{:number => :shoe => :points => :rebounds => :assists => :steals => :blocks => :slam_dunks =>}]
 end
 
 
